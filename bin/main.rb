6f45df23eba6f27ed05e5a20be5b03bdc5a172c4
Nnamdi <emelunna@gmail.com>
2020-06-27 23:20:09 +0100

    updated bin folder

---
+++ bin/main.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+# require_relative '../lib/bot.rb'
+# require_relative '../lib/motivate.rb'
+
+puts 'Hello! , Welcome to weather forcast telegram bot'
+puts '----------------------------------------------'
+puts 'This is a bot to help you know the weather forcast and condition for different cities'
+puts 'whenever you want.'
+sleep(0.8)
+puts '----------------------------------------------'
+puts '...loading'
+puts 'bot is now online... use ctrl-c to stop the bot'
