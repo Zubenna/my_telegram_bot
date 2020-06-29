13e18c0cd0ee47fb7b12166dfb3c52931a421736
Nnamdi <emelunna@gmail.com>
2020-06-29 14:45:06 +0100

    updated GemFile

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/ruby
-# require_relative '../lib/bot.rb'
-# require_relative '../lib/motivate.rb'
-
+require_relative '../lib/bot'
+require_relative '../lib/weather_info'
 puts 'Hello! , Welcome to weather forcast telegram bot'
 puts '----------------------------------------------'
 puts 'This is a bot to help you know the weather forcast and condition for different cities'
@@ -10,3 +9,4 @@ sleep(0.8)
 puts '----------------------------------------------'
 puts '...loading'
 puts 'bot is now online... use ctrl-c to stop the bot'
+Bot.new
--- lib/bot.rb
+++ lib/bot.rb
@@ -1,17 +1,26 @@
-require 'rubygems'
+# require 'rubygems'
 require 'telegram/bot'
+require_relative 'weather_info'
 
-token = 'replace-me-with-your-real-token'
+class Bot
+  def initialize
+    token = '1307559213:AAG1e-4Ep8NBCA6__SNQvBdLIym9aPMkKCk'
 
-Telegram::Bot::Client.run(token) do |bot|
+  Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}!")
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}, welcome to weather forcaste chat bot created by Nnamdi Emelu, the bot is to keep updated about weather forcaste of ceratin cities. Use  /start to start the bot,  /end to end the bot, /forcast to get a weather focaste of diffrent cities anytime you want it.")
     when '/end'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}!")
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}, date: message.date")
+    when '/forcast'
+        values = WeatherForcast.new
+        # value = 
+        # bot.api.send_message(chat_id: message.chat.id, text: "#{value['text']}", date: message.date) 
     else
-      bot.api.send_message(chat_id: message.chat.id, text: "I don't understand you :(")
+      # bot.api.send_message(chat_id: message.chat.id, text: "Your selection is wrong:")
     end
   end
+ end
+ end
 end
