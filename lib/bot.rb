b2cb84bade3fb81b87a7ba485e490797491289a2
Nnamdi <emelunna@gmail.com>
2020-07-01 13:40:44 +0100

    Disabled Metrics/AbcSize,MethodLength,Layout/LineLength

--- lib/bot.rb
+++ lib/bot.rb
@@ -3,43 +3,44 @@ require 'telegram/bot'
 require_relative 'weather_info.rb'
 
 class Bot
-# rubocop:disable Metrics/AbcSize,Metrics/MethodLength,Layout/LineLength
+  # rubocop:disable Metrics/AbcSize,Metrics/MethodLength,Layout/LineLength
   def initialize
     token = '1307559213:AAG1e-4Ep8NBCA6__SNQvBdLIym9aPMkKCk'
     Telegram::Bot::Client.run(token) do |bot|
       bot.listen do |message|
         case message.text
         when '/start'
-        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}, welcome to weather information bot created by Nnamdi Emelu, this bot will give you current weather information about your selected city. It will also display daily weather forcast of your selected city", date: message.date)
-        bot.api.send_message(chat_id: message.chat.id, text: 'Use /start to start the bot')
-        bot.api.send_message(chat_id: message.chat.id, text: 'Use /stop to stop the bot')
-        bot.api.send_message(chat_id: message.chat.id, text: 'Use /forcast to display next 3hours weather forcast in your city')
-        bot.api.send_message(chat_id: message.chat.id, text: 'Use /list_city to list sample cities')
-        bot.api.send_message(chat_id: message.chat.id, text: 'Use /current to type in your city following the format, [City, Country code]. see list_city for example')
-        bot.api.send_message(chat_id: message.chat.id, text: 'Make one selection, stop bot after display', date: message.date)
+          bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}, welcome to weather information bot created by Nnamdi Emelu, this bot will give you current weather information about your selected city. It will also display daily weather forcast of your selected city", date: message.date)
+          bot.api.send_message(chat_id: message.chat.id, text: 'Use /start to start the bot')
+          bot.api.send_message(chat_id: message.chat.id, text: 'Use /stop to stop the bot')
+          bot.api.send_message(chat_id: message.chat.id, text: 'Use /forcast to display next 3hours weather forcast in your city')
+          bot.api.send_message(chat_id: message.chat.id, text: 'Use /list_city to list sample cities')
+          bot.api.send_message(chat_id: message.chat.id, text: 'Use /current to type in your city following the format, [City, Country code]. see list_city for example')
+          bot.api.send_message(chat_id: message.chat.id, text: 'Make one selection, stop bot after display', date: message.date)
         when '/stop'
-        bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}", date: message.date)
+          bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}", date: message.date)
         when '/list_city'
-        values = WeatherForcast.new
-        list = values.city_list
-        bot.api.send_message(chat_id: message.chat.id, text: list.to_s, date: message.date)
+          values = WeatherForcast.new
+          list = values.city_list
+          bot.api.send_message(chat_id: message.chat.id, text: list.to_s, date: message.date)
         when '/current'
-        bot.api.send_message(chat_id: message.chat.id, text: 'Enter your city and country code to get current weather information')
-        bot.api.send_message(chat_id: message.chat.id, text: 'Example Lagos, NG')
-        get_city(bot)
+          bot.api.send_message(chat_id: message.chat.id, text: 'Enter your city and country code to get current weather information')
+          bot.api.send_message(chat_id: message.chat.id, text: 'Example Lagos, NG')
+          get_city(bot)
         when '/forcast'
-        bot.api.send_message(chat_id: message.chat.id, text: 'Enter your city and country code to get daily weather information')
-        bot.api.send_message(chat_id: message.chat.id, text: 'Example Lagos, NG')
-        forcast_weather(bot)
+          bot.api.send_message(chat_id: message.chat.id, text: 'Enter your city and country code to get daily weather information')
+          bot.api.send_message(chat_id: message.chat.id, text: 'Example Lagos, NG')
+          forcast_weather(bot)
         else
-        bot.api.send_message(chat_id: message.chat.id, text: 'Your selection is wrong:')
+          bot.api.send_message(chat_id: message.chat.id, text: 'Your selection is wrong:')
         end
       end
     end
   end
-# rubocop:enable Metrics/AbcSize,Metrics/MethodLength,Layout/LineLength
+  # rubocop:enable Metrics/AbcSize,Metrics/MethodLength,Layout/LineLength
+
   def get_city(bot)
-      bot.listen do |message|
+    bot.listen do |message|
       case message
       when Telegram::Bot::Types::Message
         request_type = 'current'
@@ -51,45 +52,46 @@ class Bot
     end
   end
 
-    def forcast_weather(bot)
-      bot.listen do |message|
-        case message
-        when Telegram::Bot::Types::Message
-          request_type = 'daily'
-          temp_request = 'current'
-          input_city = message.text
-          values = WeatherForcast.new
-          response = values.request_weather_data(input_city, temp_request)
-          lat = response['coord']['lat']
-          lon = response['coord']['lon']
-          response_2 = values.request_weather_data(input_city, request_type, lat, lon)
-          display_daily(message, response_2, bot)
-        end
+  def forcast_weather(bot)
+    bot.listen do |message|
+      case message
+      when Telegram::Bot::Types::Message
+        request_type = 'daily'
+        temp_request = 'current'
+        input_city = message.text
+        values = WeatherForcast.new
+        response = values.request_weather_data(input_city, temp_request)
+        lat = response['coord']['lat']
+        lon = response['coord']['lon']
+        response_2 = values.request_weather_data(input_city, request_type, lat, lon)
+        display_daily(message, response_2, bot)
       end
     end
+  end
     
-    def display_current(message, response, bot)
-        bot.api.send_message(chat_id: message.chat.id, text: 'Current weather information', date: message.date)
-        bot.api.send_message(chat_id: message.chat.id, text: "Country code: #{response['sys']['country']}", date: message.date)
-        bot.api.send_message(chat_id: message.chat.id, text: "City: #{response['name']}", date: message.date)
-        bot.api.send_message(chat_id: message.chat.id, text: "Tempreture: #{'%.2f' % (response['main']['temp'] - 273.15).to_s} Degree Celsius", date: message.date)
-        bot.api.send_message(chat_id: message.chat.id, text: "Pressure: #{response['main']['pressure']} hPa", date: message.date)
-        bot.api.send_message(chat_id: message.chat.id, text: "Humidity: #{response['main']['humidity']}%", date: message.date)
-        bot.api.send_message(chat_id: message.chat.id, text: "Visibility: #{response['visibility']}", date: message.date)
-        bot.api.send_message(chat_id: message.chat.id, text: "Wind Speed: #{response['wind']['speed']}m/s", date: message.date)
-        bot.api.send_message(chat_id: message.chat.id, text: "Longitude: #{response['coord']['lon']}", date: message.date)
-        bot.api.send_message(chat_id: message.chat.id, text: "Latitude: #{response['coord']['lat']}", date: message.date)
-    end
-    def display_daily(message, response, bot)
-      bot.api.send_message(chat_id: message.chat.id, text: 'Weather forcast for today', date: message.date)
-      bot.api.send_message(chat_id: message.chat.id, text: "Timezone: #{response['timezone']}", date: message.date)
-      bot.api.send_message(chat_id: message.chat.id, text: "Cloud: #{response['current']['clouds']}%", date: message.date)
-      bot.api.send_message(chat_id: message.chat.id, text: "Tempreture: #{'%.2f' % (response['current']['temp'] - 273.15).to_s} Degree Celsius", date: message.date)
-      bot.api.send_message(chat_id: message.chat.id, text: "Pressure: #{response['current']['pressure']} hPa", date: message.date)
-      bot.api.send_message(chat_id: message.chat.id, text: "Humidity: #{response['current']['humidity']}%", date: message.date)
-      bot.api.send_message(chat_id: message.chat.id, text: "Visibility: #{response['current']['visibility']}", date: message.date)
-      bot.api.send_message(chat_id: message.chat.id, text: "Wind Speed: #{response['current']['wind_speed']}m/s", date: message.date)
-      bot.api.send_message(chat_id: message.chat.id, text: "Longitude: #{response['lat']}", date: message.date)
-      bot.api.send_message(chat_id: message.chat.id, text: "Latitude: #{response['lon']}", date: message.date)
+  def display_current(message, response, bot)
+    bot.api.send_message(chat_id: message.chat.id, text: 'Current weather information', date: message.date)
+    bot.api.send_message(chat_id: message.chat.id, text: "Country code: #{response['sys']['country']}", date: message.date)
+    bot.api.send_message(chat_id: message.chat.id, text: "City: #{response['name']}", date: message.date)
+    bot.api.send_message(chat_id: message.chat.id, text: "Tempreture: #{'%.2f' % (response['main']['temp'] - 273.15).to_s} Degree Celsius", date: message.date)
+    bot.api.send_message(chat_id: message.chat.id, text: "Pressure: #{response['main']['pressure']} hPa", date: message.date)
+    bot.api.send_message(chat_id: message.chat.id, text: "Humidity: #{response['main']['humidity']}%", date: message.date)
+    bot.api.send_message(chat_id: message.chat.id, text: "Visibility: #{response['visibility']}", date: message.date)
+    bot.api.send_message(chat_id: message.chat.id, text: "Wind Speed: #{response['wind']['speed']}m/s", date: message.date)
+    bot.api.send_message(chat_id: message.chat.id, text: "Longitude: #{response['coord']['lon']}", date: message.date)
+    bot.api.send_message(chat_id: message.chat.id, text: "Latitude: #{response['coord']['lat']}", date: message.date)
+  end
+
+  def display_daily(message, response, bot)
+    bot.api.send_message(chat_id: message.chat.id, text: 'Weather forcast for today', date: message.date)
+    bot.api.send_message(chat_id: message.chat.id, text: "Timezone: #{response['timezone']}", date: message.date)
+    bot.api.send_message(chat_id: message.chat.id, text: "Cloud: #{response['current']['clouds']}%", date: message.date)
+    bot.api.send_message(chat_id: message.chat.id, text: "Tempreture: #{'%.2f' % (response['current']['temp'] - 273.15).to_s} Degree Celsius", date: message.date)
+    bot.api.send_message(chat_id: message.chat.id, text: "Pressure: #{response['current']['pressure']} hPa", date: message.date)
+    bot.api.send_message(chat_id: message.chat.id, text: "Humidity: #{response['current']['humidity']}%", date: message.date)
+    bot.api.send_message(chat_id: message.chat.id, text: "Visibility: #{response['current']['visibility']}", date: message.date)
+    bot.api.send_message(chat_id: message.chat.id, text: "Wind Speed: #{response['current']['wind_speed']}m/s", date: message.date)
+    bot.api.send_message(chat_id: message.chat.id, text: "Longitude: #{response['lat']}", date: message.date)
+    bot.api.send_message(chat_id: message.chat.id, text: "Latitude: #{response['lon']}", date: message.date)
   end
 end
