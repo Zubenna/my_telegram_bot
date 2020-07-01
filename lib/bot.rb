1912fb26270e03c00a825ee930c333b2ef2ea37a
Nnamdi <emelunna@gmail.com>
2020-07-01 01:48:14 +0100

    implemented daily weather forcast

--- lib/bot.rb
+++ lib/bot.rb
@@ -13,33 +13,81 @@ class Bot
       bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}, welcome to weather forcaste chat bot created by Nnamdi Emelu, this bot will give you current weather information about your selected city.", date: message.date)
       bot.api.send_message(chat_id: message.chat.id, text: 'Use /start to start the bot')
       bot.api.send_message(chat_id: message.chat.id, text: 'Use /stop to stop the bot')
-      bot.api.send_message(chat_id: message.chat.id, text: 'Use /disp_current to display current weather')
+      bot.api.send_message(chat_id: message.chat.id, text: 'Use /forcast to display next 3hours weather forcast in your city')
       bot.api.send_message(chat_id: message.chat.id, text: 'Use /list_city to list sample cities')
-      bot.api.send_message(chat_id: message.chat.id, text: 'Use /enter_city to type in your city following the format, [City, Country code]. see list_city for example' )
+      bot.api.send_message(chat_id: message.chat.id, text: 'Use /current to type in your city following the format, [City, Country code]. see list_city for example' )
+      bot.api.send_message(chat_id: message.chat.id, text: 'Make one selection, stop bot after display', date: message.date)
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}", date: message.date)
     when '/list_city'
       values = WeatherForcast.new
       list = values.city_list
       bot.api.send_message(chat_id: message.chat.id, text: "#{list}", date: message.date)
-    when '/disp_current'
-      values = WeatherForcast.new
-      response = values.request_weather_data
-      bot.api.send_message(chat_id: message.chat.id, text: "Country code: #{response['sys']['country']}", date: message.date)
-      bot.api.send_message(chat_id: message.chat.id, text: "City: #{response['name']}", date: message.date)
-      bot.api.send_message(chat_id: message.chat.id, text: "Tempreture: #{'%.2f' % (response['main']['temp'] - 273.15).to_s} Degree Celsius", date: message.date)
-      bot.api.send_message(chat_id: message.chat.id, text: "Pressure: #{response['main']['pressure']} hPa", date: message.date)
-      bot.api.send_message(chat_id: message.chat.id, text: "Humidity: #{response['main']['humidity']}%", date: message.date)
-      bot.api.send_message(chat_id: message.chat.id, text: "Visibility: #{response['visibility']}", date: message.date)
-      bot.api.send_message(chat_id: message.chat.id, text: "Wind Speed: #{response['wind']['speed']}m/s", date: message.date)
-      bot.api.send_message(chat_id: message.chat.id, text: "Longitude: #{response['coord']['lon']}", date: message.date)
-      bot.api.send_message(chat_id: message.chat.id, text: "Latitude: #{response['coord']['lat']}", date: message.date)
-    # when '/enter_city'
-    #   input = message.text
+    when '/current'
+      bot.api.send_message(chat_id: message.chat.id, text: 'Enter your city and country code to get current weather information')
+      bot.api.send_message(chat_id: message.chat.id, text: 'Example Lagos, NG')
+      get_city(bot)
+    when '/forcast'
+      bot.api.send_message(chat_id: message.chat.id, text: 'Enter your city and country code to get daily weather information')
+      bot.api.send_message(chat_id: message.chat.id, text: 'Example Lagos, NG')
+      forcast_weather(bot)
     else
       bot.api.send_message(chat_id: message.chat.id, text: "Your selection is wrong:")
     end
+    end
+ end
+ end
+  def get_city(bot)
+      bot.listen do |message|
+      case message
+      when Telegram::Bot::Types::Message
+        request_type = 'current'
+        input_city = message.text
+        values = WeatherForcast.new
+        response = values.request_weather_data(input_city, request_type)
+        display_current(message, response, bot)
+      end
+    end
   end
- end
- end
+    def forcast_weather(bot)
+      bot.listen do |message|
+        case message
+        when Telegram::Bot::Types::Message
+          request_type = 'daily'
+          temp_request = 'current'
+          input_city = message.text
+          values = WeatherForcast.new
+          response = values.request_weather_data(input_city, temp_request)
+          lat = response['coord']['lat']
+          lon = response['coord']['lon']
+          response_2 = values.request_weather_data(input_city, request_type, lat, lon)
+          display_daily(message, response_2, bot)
+        end
+      end
+    end
+    
+    def display_current(message, response, bot)
+        bot.api.send_message(chat_id: message.chat.id, text: 'Current weather information', date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: "Country code: #{response['sys']['country']}", date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: "City: #{response['name']}", date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: "Tempreture: #{'%.2f' % (response['main']['temp'] - 273.15).to_s} Degree Celsius", date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: "Pressure: #{response['main']['pressure']} hPa", date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: "Humidity: #{response['main']['humidity']}%", date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: "Visibility: #{response['visibility']}", date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: "Wind Speed: #{response['wind']['speed']}m/s", date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: "Longitude: #{response['coord']['lon']}", date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: "Latitude: #{response['coord']['lat']}", date: message.date)
+    end
+    def display_daily(message, response, bot)
+      bot.api.send_message(chat_id: message.chat.id, text: 'Weather forcast for today', date: message.date)
+      bot.api.send_message(chat_id: message.chat.id, text: "Timezone: #{response['timezone']}", date: message.date)
+      bot.api.send_message(chat_id: message.chat.id, text: "Cloud: #{response['current']['clouds']}%", date: message.date)
+      bot.api.send_message(chat_id: message.chat.id, text: "Tempreture: #{'%.2f' % (response['current']['temp'] - 273.15).to_s} Degree Celsius", date: message.date)
+      bot.api.send_message(chat_id: message.chat.id, text: "Pressure: #{response['current']['pressure']} hPa", date: message.date)
+      bot.api.send_message(chat_id: message.chat.id, text: "Humidity: #{response['current']['humidity']}%", date: message.date)
+      bot.api.send_message(chat_id: message.chat.id, text: "Visibility: #{response['current']['visibility']}", date: message.date)
+      bot.api.send_message(chat_id: message.chat.id, text: "Wind Speed: #{response['current']['wind_speed']}m/s", date: message.date)
+      bot.api.send_message(chat_id: message.chat.id, text: "Longitude: #{response['lat']}", date: message.date)
+      bot.api.send_message(chat_id: message.chat.id, text: "Latitude: #{response['lon']}", date: message.date)
+  end
 end
--- lib/weather_info.rb
+++ lib/weather_info.rb
@@ -4,16 +4,24 @@ require 'uri'
 require 'net/http'
 require_relative 'bot'
 class WeatherForcast
-    def request_weather_data
-     city = 'Madrid, ES'
-     url = "http://api.openweathermap.org/data/2.5/weather?q=#{city}&APPID=4c726c2ad8e25995fa54253e43f9b966"
-     uri = URI(url)
-     response = Net::HTTP.get(uri)
-     response = JSON.parse(response)
-     response
+    def request_weather_data(city, data, lat=nil, lon=nil)
+      url = "http://api.openweathermap.org/data/2.5/weather?q=#{city}&APPID=4c726c2ad8e25995fa54253e43f9b966" if data.eql?('current')
+      url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{lon}&
+      exclude=current,minutely,hourly&appid=4c726c2ad8e25995fa54253e43f9b966" if data.eql?('daily')
+      uri = URI(url)
+      response = Net::HTTP.get(uri)
+      response = JSON.parse(response)
+      response
     end
     def city_list
-         city = ['Lagos, NG', 'Port Harcourt, NG', 'Abuja, NG', 'Madrid, ES', 'London, UK']
-         city
+      city = ['Lagos, NG', 'Port Harcourt, NG', 'Abuja, NG', 'Madrid, ES', 'London, UK']
+      city
     end
 end
+
+
+
+
+
+
+
