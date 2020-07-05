5163d119825b9962f7806fe32042a674042ad0af
Nnamdi <emelunna@gmail.com>
2020-07-05 15:35:50 +0100

    Perfomed git rebase on commit messages

---
+++ rebased_commit.rb
@@ -0,0 +1,28 @@
+reword f3a6866 Added prerequisite for using bot in readme file
+reword 624805a Added how to use to readme file
+reword 054355e Removed extra space from line 40 of bot.rb
+reword d042afc Disabled Metrics/AbcSize,MethodLength,Layout/LineLength
+reword 04a9b6d Corrected identation error in line 54 to 64 of bot.rb file
+reword 2d9e3f2 Changed all single quote to double quote in weather_info_spec.rb
+reword 4a59508 Corrected more stickler  error
+pick c822ca5 modified bot logic
+pick e2cf83f added new line at the end of bot.rb
+
+# Rebase 1d0e76e..e2cf83f onto 1d0e76e (9 commands)
+#
+# Commands:
+# p, pick = use commit
+# r, reword = use commit, but edit the commit message
+# e, edit = use commit, but stop for amending
+# s, squash = use commit, but meld into previous commit
+# f, fixup = like "squash", but discard this commit's log message
+# x, exec = run command (the rest of the line) using shell
+# d, drop = remove commit
+#
+# These lines can be re-ordered; they are executed from top to bottom.
+#
+# If you remove a line here THAT COMMIT WILL BE LOST.
+#
+# However, if you remove everything, the rebase will be aborted.
+#
+# Note that empty commits are commented out
