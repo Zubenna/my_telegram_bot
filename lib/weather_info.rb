dbf386375839af6ea7f2590f1d776bea2869547a
Nnamdi <emelunna@gmail.com>
2020-07-06 03:15:52 +0100

    Added get_weather_data method in weather_info file

--- lib/weather_info.rb
+++ lib/weather_info.rb
@@ -11,14 +11,19 @@ class WeatherForcast
       url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{lon}&
       exclude=current,minutely,hourly&appid=4c726c2ad8e25995fa54253e43f9b966"
     end
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
-    response = JSON.parse(response)
-    response
+    get_weather_data(url)
   end
 
   def city_list
     city = ['Lagos, NG', 'Port Harcourt, NG', 'Abuja, NG', 'Madrid, ES', 'London, UK']
     city
+  end
+
+  private
+  def get_weather_data(url)
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    response = JSON.parse(response)
+    response
   end
 end
